6308c3f6541790910977cca4a218f1c13e408c7c
Petr Starostin <starostin.petr@yandex.ru>
2023-03-24 19:06:28 +0400

    Second task

--- Program.cs
+++ Program.cs
@@ -13,3 +13,17 @@
 // int grade = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine($"{num} в степени {grade} : {Pownum(num, grade)}");
 
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int SumofNumbers(int num)
+{
+    int result = 0;
+    while(num > 0)
+    {
+        result += num % 10; 
+        num = num / 10 ;
+    }
+    return result;
+}
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма числа {num}: {SumofNumbers(num)}");
