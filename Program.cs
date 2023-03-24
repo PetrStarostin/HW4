1eecece65221ae9ef6500ddf198cb19433f9d001
Petr Starostin <starostin.petr@yandex.ru>
2023-03-24 18:42:33 +0400

    first task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// int Pownum(int num , int grade)
+// {
+//     for(int i = 1; i < grade ; i++)
+//     {
+//         num *= num;
+//     }
+//     return num ; 
+// }
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите степень: ");
+// int grade = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"{num} в степени {grade} : {Pownum(num, grade)}");
+
